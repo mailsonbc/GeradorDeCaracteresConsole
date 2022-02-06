13067c597de519d3033bcdd0673672635ebaf013
Mailson Barreto <mailsonbc@gmail.com>
2022-02-06 02:43:48 -0300

    Gerador de caracteres aleatórios

---
+++ TesteGeradorAleatorio/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TesteGeradorAleatorio
+{
+    class Program
+    {
+        public class Geradora
+        {
+            Random gera = new Random();
+
+            string[] simbolos = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+                "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+                "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5",
+                "6", "7", "8", "9", "0", "!", "@", "#", "$", "%", "&", "*", "+", "-", "/"};
+            string senha = "";
+            public void Gerar()
+            {
+                while (senha.Length < 15)
+                {
+                    int g = gera.Next(0, 71);
+                    senha = senha + simbolos[g];                    
+                }
+
+                Console.WriteLine(senha);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Geradora geradora = new Geradora();
+            geradora.Gerar();
+        }
+    }
+}
